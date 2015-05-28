d2997c580694a669d9d067c8a6d7b1eeb77ed333
Victor Hugo Marques <87victorhugo@gmail.com>
2015-05-28 16:08:05 -0300

    react start learning

--- frame8.ui/src/js/components/child.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-let Child =
-    React.createClass({
-        render: function() {
-            return <h1> The Child Node </h1>
-        }
-    });
-module.exports = Child;
